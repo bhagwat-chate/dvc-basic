f2401f600e195a7abfdc51706de6fca70cfad942
bhagwat-chate <bhagwat.chate25@gmail.com>
2021-09-26 14:46:58 +0530

    README.md updated

--- README.md
+++ README.md
@@ -10,5 +10,13 @@ pip install dvc
 ```
 
 ```bash
+dvc init
+```
+
+```bash
 dvc repro
+```
+
+```bash
+Add files stage_01.py stage_02.py stage_03.py
 ```
--- stage_01.py
+++ stage_01.py
@@ -1,4 +1,5 @@
-text = 'Hello Data Version Control'
+text = 'Hello Data Version Control.'
+text = text + ' DVC is working!'
 
 with open('artifacts01.txt', 'w') as f:
     f.write(text)
